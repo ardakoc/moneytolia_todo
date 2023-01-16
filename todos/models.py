2258e5d12877c16fbb75a6ba59446b89d652e3e5
Arda Koç <ardakoc@github.com>
2023-01-16 09:18:31 +0300

    added boolean field named 'completed' to the Todo model.

--- todos/models.py
+++ todos/models.py
@@ -8,6 +8,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title + ' (' + str(self.user) + ')'
