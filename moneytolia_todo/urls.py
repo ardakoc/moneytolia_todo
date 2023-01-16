b7b2371603fd31403efa8f952de27bb027e6e955
Arda Koç <ardakoc@github.com>
2023-01-16 13:56:01 +0300

    updated permission classes of documentation view.

--- moneytolia_todo/urls.py
+++ moneytolia_todo/urls.py
@@ -1,5 +1,6 @@
-from rest_framework.authtoken import views
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 
 from django.contrib import admin
 from django.urls import path, include
@@ -7,5 +8,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
-    path('', include_docs_urls(title='Moneytolia To-Do API'))
+    path('', include_docs_urls(
+        title='Moneytolia To-Do API',
+        permission_classes=(AllowAny,))),
 ]
